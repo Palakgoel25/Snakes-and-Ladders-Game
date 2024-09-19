205a1170a58bbfaebc3b2bb641b92367ea9d694c
Palak Goel <88661495+Palakgoel25@users.noreply.github.com>
2024-09-20 01:15:19 +0530

    Update main.cpp
    Used a class for Player instead of string

--- main.cpp
+++ main.cpp
@@ -1,57 +1,92 @@
 #include<iostream>
 #include<string>
 using namespace std;
-void func(int &d,int &pos,string s)
-{cout<<"You rolled "<<d<<endl;
-pos=pos + d;
-if(pos>25)
-pos=pos-d;
-if(pos==4)
+
+class Player
+{ public :
+string name;
+ int pos;
+Player(string n =0)
+ {
+    name = n;
+    pos =0;
+ }
+
+};
+void func(int &d,Player &p)
+{cout<<p.name<<" rolled "<<d<<endl;
+p.pos= p.pos + d;
+if(p.pos>25)
+p.pos=p.pos-d;
+if(p.pos==4)
 {cout<<"Climb up the ladder!"<<endl;
-    pos=15;}
-if(pos==12)
+    p.pos=15;}
+if(p.pos==12)
 {cout<<"Climb up the ladder!"<<endl;
-    pos=21;}
-if(pos==24)
+    p.pos=21;}
+if(p.pos==24)
 {cout<<"oh,no!SNAKE"<<endl;
-    pos=5;}
-if(pos==11)
+    p.pos=5;}
+if(p.pos==11)
 {cout<<"oh,no!SNAKE"<<endl;
-    pos=2;}
-if(pos==25)
+    p.pos=2;}
+if(p.pos==25)
 return;
-cout<<s<<"'s position is "<<pos<<endl<<endl;
+cout<<"Position of "<<p.name<<" is "<<p.pos<<endl;
 return;
 }
+
 int main()
-{int d1,d2,pos1=0,pos2=0;
-string Player;
-char c;
+{int d;
 cout<<"Snakes and ladders game"<<endl;
-cout<<"ENTER 1 TO PLAY"<<endl;
-int p;
-cin>>p;
-if(p==1)
-{while(pos1!=25 && pos2!=25)
-{cout<<"Player 1's turn!"<<" Press p to roll dice"<<endl;
+cout<<"Enter 1st player's name"<<endl;
+string n;
+cin>>n;
+Player p1(n);
+cout<<"Enter 2nd player's name"<<endl;
+cin>>n;
+Player p2(n);
+
+cout<<"Press 1 to start the game"<<endl;
+int s;
+cin>>s;
+if(s==1)
+{
+
+while(p1.pos!=25 && p2.pos!=25)
+{ 
+    cout<<p1.name<<"'s turn!"<<" Press p to roll dice"<<endl;
+    char c;
 cin>>c;
-Player="Player 1";
-d1=1+(rand()%6);
-func(d1,pos1,Player);
-if(pos1==25)
-    break;
-cout<<"Player's 2 turn!"<<"press p to roll dice"<<endl;
-Player="Player 2";
+while(c != 'p')
+{cout<<"Roll again!"<<endl;
+cin>>c;}
+
+int d;
+d=1+(rand()%6);
+func(d,p1);
+if(p1.pos==25)
+{
+cout<<p1.name<<" is the winner"<<endl;
+break;}
+
+ cout<<p2.name<<"'s turn!"<<" Press p to roll dice"<<endl;
+
 cin>>c;
-d2=1+(rand()%6);
-func(d2,pos2,Player);
-if(pos2==25)
-    break;}
-if(pos1==25)
-{cout<<"PLAYER 1 IS THE  WINNER"<<endl;
-return 0;}
-if(pos2==25)
-{cout<<"PLAYER 2 IS THE  WINNER";
-return 0;}
+while(c != 'p')
+{cout<<"Roll again!"<<endl;
+cin>>c;}
+d=1+(rand()%6);
+
+func(d,p2);
+if(p2.pos==25)
+{
+cout<<p2.name<<" is the winner"<<endl;
+break;}
+
 }
+
 }
+return 0;
+}
+
